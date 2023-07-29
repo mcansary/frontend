a9622d8e452f63096f103d68cb6d8571be6d8079
taro <taro@example.com>
2023-07-29 16:00:41 +0000

    フロントエンド08の課題を追加

---
+++ frontend8.js
@@ -0,0 +1,12 @@
+function triangle(p1, p2) {
+    let p= p1 * p2 / 2;
+    return p;
+}
+console.log(triangle(10, 20));
+
+
+var triangle = function(p1, p2) {
+    let p = p1 * p2 /2;
+    return p;
+}
+console.log(triangle(10, 20));
