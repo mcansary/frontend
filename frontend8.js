5203b3b4dedf4fcf3abfb136733545fb552c7d7d
taro <taro@example.com>
2023-09-22 08:05:48 +0000

    【フロントエンド】09課題を追加

--- frontend8.js
+++ frontend8.js
@@ -1,12 +1,14 @@
-function triangle(p1, p2) {
+function func_triangle(p1, p2) {
+//       ^^^^^^^^^^^^^ 関数名
     let p= p1 * p2 / 2;
     return p;
 }
-console.log(triangle(10, 20));
+console.log(func_triangle(10, 20));
 
 
-var triangle = function(p1, p2) {
+var var_triangle = function(p1, p2) {
+//  ^^^^^^^^^^^^ 変数名
     let p = p1 * p2 /2;
     return p;
 }
-console.log(triangle(10, 20));
+console.log(var_triangle(10, 20));
---
+++ test.js
@@ -0,0 +1,27 @@
+var test1 = 10;
+let test2 = 20;
+
+console.log(test1);
+console.log(test2);
+
+function func01() {
+    console.log(test1);
+    console.log(test2);
+    
+    let test3 = 30;
+    console.log(test3);
+    var test4 = 40;
+    console.log(test4);
+}
+console.log(test3);
+console.log(test4);
+if (true) {
+    var test5 = 40;
+    console.log(test5);
+    
+}
+console.log(test5);
+
+var test6 = 100;
+console.log(test6);
+
