48555a27d6e171776ac979a8fae3ed5ad041246f
taro <taro@example.com>
2023-10-01 06:41:40 +0000

    【フロントエンド】11課題を追加

---
+++ frontend11.js
@@ -0,0 +1,8 @@
+// 以下に実装してください
+let e1 = document.getElementById('p1');
+let e2 = document.getElementById('p2');
+let e3 = document.getElementById('p3');
+
+e1.outerHTML = '<input type="input" value="変更">';
+e2.innerText = 'テキスト文字列';
+e3.id = 'p5'; 
